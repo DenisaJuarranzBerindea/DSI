b5a1db7e6d595638aaa160cb29e91aa099e28acf
M00n-Hun7r3ss <denisaju@ucm.es>
2024-04-28 12:59:13 +0200

    Fin 4a

--- Unity/DSI_Lab4/Assets/Lab4.cs
+++ Unity/DSI_Lab4/Assets/Lab4.cs
@@ -11,9 +11,21 @@ public class Lab4 : MonoBehaviour
 
         Label Mario = root.Q<Label>("NameMario");
         Mario.text = @"<gradient=""Mario""><i> MARIO </i></gradient>";
-        
 
+        Label Luigi = root.Q<Label>("NameLuigi");
+        Luigi.text = @"<gradient=""Luigi""><i> LUIGI </i></gradient>";
 
+        Label Peach = root.Q<Label>("NamePeach");
+        Peach.text = @"<gradient=""Peach""><i> PEACH </i></gradient>";
+
+        Label Estela = root.Q<Label>("NameEstela");
+        Estela.text = @"<gradient=""Estela""><i> ESTELA </i></gradient>";
+
+        Label SKart = root.Q<Label>("NameSKart");
+        SKart.text = @"<gradient=""Special""><i> SPECIAL KART </i></gradient>";
+
+        Label SBike = root.Q<Label>("NameSBike");
+        SBike.text = @"<gradient=""Special""><i> SPECIAL BIKE </i></gradient>";
 
     }
 
